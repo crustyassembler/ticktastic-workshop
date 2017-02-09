c4bc868c2df6143efb2900c06bb99660ab539f98
Erik Kaighan <crustyassembler@users.noreply.github.com>
2017-02-09 17:03:04 +0000

    Write unsanitised user input from localstorage to the DOM

--- public/main.js
+++ public/main.js
@@ -1,17 +1,42 @@
-var $form = $('#ticket-form')
+var tt = {};
 
-$form.on('submit', function(e) {
-    var values = {}, $inputs;
-    
-    e.preventDefault();
-    $inputs = $form.find('select, textarea, input');
-    $inputs.each(function(key, el) {
-	var name = el.name;
-	var value = el.value;
-	if(name) {
-	    values[name] = value;
-	}
+tt.form = function($form) {
+    $form.on('submit', function(e) {
+	var values = {}, $inputs;
+	
+	e.preventDefault();
+	$inputs = $form.find('select, textarea, input');
+	$inputs.each(function(key, el) {
+	    var name = el.name;
+	    var value = el.value;
+	    if(name) {
+		values[name] = value;
+	    }
+	});
+
+	window.localStorage.setItem('ticket', JSON.stringify(values));
     });
+};
 
-    window.localStorage.setItem('ticket', JSON.stringify(values));
+tt.list = function($list) {
+    var values = JSON.parse(window.localStorage.getItem('ticket'));
+    var title = values.details;
+    var user = values.name;
+    
+    var template = '<li class="ticket-list__item"><h4 class="ticket-list__title">'+title+'</h4><span class="ticket-list__user">'+user+'</span></li>';
+
+    if(values) {
+	$list.html(template);
+    }
+};
+
+tt.main = function() {
+    var $form = $('#ticket-form');
+    var $list = $('#ticket-list');
+    tt.form($form);
+    tt.list($list);
+};
+
+$(document).ready(function() {
+    tt.main();
 });

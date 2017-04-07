5e4207aa2b5eb8eeec0170d5cb5821ca8eef580b
Erik Kaighan <crustyassembler@users.noreply.github.com>
2017-04-07 14:39:46 +0100

    Commit all my homework

--- public/main.js
+++ public/main.js
@@ -1,42 +1,89 @@
 var tt = {};
 
-tt.form = function($form) {
-    $form.on('submit', function(e) {
-	var values = {}, $inputs;
-	
-	e.preventDefault();
-	$inputs = $form.find('select, textarea, input');
-	$inputs.each(function(key, el) {
-	    var name = el.name;
-	    var value = el.value;
-	    if(name) {
-		values[name] = value;
-	    }
-	});
+tt.form = function($form, tickets) {
+  $form.on('submit', function(e) {
+    var values = {};
+    var $inputs;
 
-	window.localStorage.setItem('ticket', JSON.stringify(values));
+    e.preventDefault();
+    $inputs = $form.find('select, textarea, input');
+      $inputs.each(function(key, el) {
+      var name = el.name,
+          value = el.value;
+      if(name) {
+        values[name] = value;
+      }
     });
+
+    var ticketlist = [];
+    if(tickets) {
+      ticketlist = JSON.parse(tickets);
+    }
+    ticketlist.push(values);
+
+    window.localStorage.setItem(
+      'ticket',
+      JSON.stringify(ticketlist)
+    );
+
+    window.location.reload();
+  });
 };
 
-tt.list = function($list) {
-    var values = JSON.parse(window.localStorage.getItem('ticket'));
-    var title = values.details;
-    var user = values.name;
-    
-    var template = '<li class="ticket-list__item"><h4 class="ticket-list__title">'+title+'</h4><span class="ticket-list__user">'+user+'</span></li>';
+tt.gensum = function(string) {
+  var summary;
+  var truncated = false;
 
-    if(values) {
-	$list.html(template);
-    }
+  if(string.indexOf('\n') != -1) {
+    summary = string.split('\n')[0];
+    truncated = true;
+  } else {
+    summary = string;
+  }
+
+  if(summary.length > 35) {
+    summary = summary.substr(0, 35);
+    truncated = true;
+  }
+
+  if(truncated) {
+    summary = summary + '\u2026';
+  }
+  return summary;
+}
+
+tt.list = function($list, values) {
+  var tickets = JSON.parse(values);
+
+  for(var x in tickets) {
+    var current = tickets[x],
+        title   = tt.gensum(current.details),
+        user    = current.name
+
+    var template = (`
+      <li class="ticket-list__item-${x}">
+        <h4 class="ticket-list__title"></h4>
+        <span class="ticket-list__user"></span>
+      </li>
+    `);
+
+    $list.append(template);
+    var $current = $(`.ticket-list__item-${x}`);
+    $current.children('.ticket-list__title').text(title);
+    $current.children('.ticket-list__user').text(user);
+  }
 };
 
 tt.main = function() {
-    var $form = $('#ticket-form');
-    var $list = $('#ticket-list');
-    tt.form($form);
-    tt.list($list);
+  var $form = $('#ticket-form'),
+      $list = $('#ticket-list');
+  var tickets = window.localStorage.getItem('ticket');
+  tt.form($form, tickets);
+  if(tickets) {
+      tt.list($list, tickets);
+  }
 };
 
 $(document).ready(function() {
-    tt.main();
+  tt.main();
 });
